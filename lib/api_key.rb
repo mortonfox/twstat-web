49c267704d121e20c9f20cea8ee1963d77a6ad64
Morton Fox <github@mortonfox.otherinbox.com>
2013-04-25 23:31:40 -0400

    erroneous substitution

--- lib/api_key.rb
+++ lib/api_key.rb
@@ -5,7 +5,7 @@ class ApiKey
     @apikey = YAML::load(Rails.root.join('config', APIKEYS_FNAME).open)[Rails.env]
     raise "No API key for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey
     raise "No consumer key for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey['consumer_key']
-    raise "No consumer secret for #{Rails.env} environment in #{APIsecretS_FNAME}" unless @apikey['consumer_secret']
+    raise "No consumer secret for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey['consumer_secret']
     raise "No callback URL for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey['callback_url']
   end
 
