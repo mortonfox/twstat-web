d9721b3ad9d447a71bc585451692297a2b93947c
Morton Fox <github@mortonfox.otherinbox.com>
2013-05-13 11:57:52 -0400

    Set session cookie expiration to 1 year.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TwstatWeb::Application.config.session_store :cookie_store, key: '_twstat-web_session'
+TwstatWeb::Application.config.session_store :cookie_store, key: '_twstat-web_session', expire_after: 1.year
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
