4942ab7d939788d8af5c8df7082cb25a28327358
Morton Fox <github@qslw.com>
2016-01-20 00:06:53 -0500

    Changes suggested by Rubocop.

--- lib/api_key.rb
+++ lib/api_key.rb
@@ -1,26 +1,20 @@
+# Helper class for reading Twitter API key file.
 class ApiKey
   APIKEYS_FNAME = 'apikeys.yml'
-
-  APIKEY_FIELDS = [
-    'consumer_key', 
-    'consumer_secret', 
-    'callback_url' 
-  ]
+  APIKEY_FIELDS = %w(consumer_key consumer_secret callback_url)
 
   def initialize
-    @apikey = YAML::load(Rails.root.join('config', APIKEYS_FNAME).open)[Rails.env]
-    raise "No API key for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey
+    @apikey = YAML.load(Rails.root.join('config', APIKEYS_FNAME).open)[Rails.env]
+    fail "No API key for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey
 
     APIKEY_FIELDS.each { |field|
-
       # Verify that the API key field exists.
-      raise "No #{field} for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey[field]
+      fail "No #{field} for #{Rails.env} environment in #{APIKEYS_FNAME}" unless @apikey[field]
 
       # Define an accessor for each API key field.
       class_eval {
         define_method(field) { @apikey[field] }
       }
-
     }
   end
 end
