2c7a3086f2b3ea816bf057ab83a5c0a23f96cd1d
Morton Fox <github@qslw.com>
2016-10-06 23:43:15 -0400

    Simplify the sorting code for by_mention, by_source and by_words

--- lib/tweet_stats.rb
+++ lib/tweet_stats.rb
@@ -202,47 +202,41 @@ class TweetStats
 
     report_data['by_mention_data'] = Hash[
       COUNT_DEFS.map { |period, _periodinfo|
-        period_counts_by_mention = @all_counts[period][:by_mention]
         [
           period,
-          period_counts_by_mention
-          .keys
-          .sort { |a, b| period_counts_by_mention[b] <=> period_counts_by_mention[a] }
-          .first(10)
-          .map
-          .with_index { |user, i| "[ '@#{user}', #{period_counts_by_mention[user]}, '#{COLORS[i % COLORS.size]}' ]" }
-          .join(',')
+          @all_counts[period][:by_mention]
+            .sort_by { |_user, count| -count }
+            .first(10)
+            .map
+            .with_index { |(user, count), i| "[ '@#{user}', #{count}, '#{COLORS[i % COLORS.size]}' ]" }
+            .join(',')
         ]
       }
     ]
 
     report_data['by_source_data'] = Hash[
       COUNT_DEFS.map { |period, _periodinfo|
-        period_counts_by_source = @all_counts[period][:by_source]
         [
           period,
-          period_counts_by_source
-          .keys
-          .sort { |a, b| period_counts_by_source[b] <=> period_counts_by_source[a] }
-          .first(10)
-          .map
-          .with_index { |source, i| "[ '#{source}', #{period_counts_by_source[source]}, '#{COLORS[i % COLORS.size]}' ]" }
-          .join(',')
+          @all_counts[period][:by_source]
+            .sort_by { |_source, count| -count }
+            .first(10)
+            .map
+            .with_index { |(source, count), i| "[ '#{source}', #{count}, '#{COLORS[i % COLORS.size]}' ]" }
+            .join(',')
         ]
       }
     ]
 
     report_data['by_words_data'] = Hash[
       COUNT_DEFS.map { |period, _periodinfo|
-        period_counts_by_word = @all_counts[period][:by_word]
         [
           period,
-          period_counts_by_word
-          .keys
-          .sort { |a, b| period_counts_by_word[b] <=> period_counts_by_word[a] }
-          .first(100)
-          .map { |word| "{text: \"#{word}\", weight: #{period_counts_by_word[word]} }" }
-          .join(',')
+          @all_counts[period][:by_word]
+            .sort_by { |_word, count| -count }
+            .first(100)
+            .map { |word, count| "{text: \"#{word}\", weight: #{count} }" }
+            .join(',')
         ]
       }
     ]
